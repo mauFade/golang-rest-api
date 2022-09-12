ebb1c5fcf1d8320bb3c42cc31fc1eabc0d0625d7
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-09-12 18:09:50 -0300

    feat: models

---
+++ config/orm.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"example.com/web-service-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect()  {
+	db, err := gorm.Open(postgres.Open("postgres://postgres:postgres@localhost:5432/postgres"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&models.User{})
+	DB = db
+}
